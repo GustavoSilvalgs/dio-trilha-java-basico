6905e8abb5141349a42758e0a96f4e4154593e25
GustavoSilvalgs <luisgust06@gmail.com>
2023-03-02 12:20:35 -0300

    enviando explicações sobre operadores e variaveis

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,16 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+
+        boolean condicao1=true;
+        boolean condicao2=false;
+
+        if(condicao1 || condicao2 == true){
+            System.out.println("As condições são verdadeira");
+        }
+        else
+            System.out.println("A condição é falsa");
+
+    }
+
+}
